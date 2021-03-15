8a6dae417c96917fa99ec8d4ed562567d6cc5e63
DaikiMasaoka <masadai.29@icloud.com>
2021-03-15 12:37:31 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'users#index'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update, :index]
 end
