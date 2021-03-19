739ce81a56069a3312f686b31d1565d06afba439
DaikiMasaoka <masadai.29@icloud.com>
2021-03-19 14:30:44 +0000

    5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
-  before_action :baria_user, only: [:edit, :destroy, :update]
+  before_action :ensure_user, only: [:edit, :update, :destroy]
   def top
   end
+
   def new
     @book = Book.new
   end
@@ -18,17 +19,26 @@ class BooksController < ApplicationController
        render :index
     end
   end
+
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to new_user_session_path
+    end
   end
+
   def update
     @book = Book.find(params[:id])
-   if @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book.id)
-   else
-      render :edit
-   end
+    if @book.user != current_user
+       redirect_to new_user_session_path
+    else
+      if @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
+    end
   end
 
   def index
@@ -40,13 +50,18 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find_by(id: @book.user_id)
+
   end
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    flash[:notice] = "Book was successfully destroyed."
-    redirect_to books_path
+    if @book.user != current_user
+       redirect_to new_user_session_path
+    else
+       @book.destroy
+       flash[:notice] = "Book was successfully destroyed."
+       redirect_to books_path
+    end
   end
 
   private
@@ -54,9 +69,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  def baria_user
-    unless Book.find(params[:id]).user.id.to_i == current_user.id
-        redirect_to books_path(current_user)
-    end
-  end
+
 end
