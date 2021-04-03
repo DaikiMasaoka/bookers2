8fc923212ce36f21b7ed0c5183845b3ad77cc5bc
DaikiMasaoka <masadai.29@icloud.com>
2021-04-03 12:47:20 +0000

    Start

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show, :edit, :update, :index]
-  resources :books, only: [:create, :index, :show, :destroy, :update, :edit]
-
+  resources :books, only: [:create, :index, :show, :destroy, :update, :edit] do
+    resource :favorites, only: [:create, :destroy]
+    
+ end
 end
